92244e0a09a33f51cf86b12e2aca793ea306e15d
Ibrahim Essam <ibrahimessam@Ibrahims-MacBook-Air.local>
2026-09-25 12:10:31 +0300

    after creating the body and food , then i made the initial movement , colored the background and objects , created the loob of the game like the movement and some checks .

--- snake_game.py
+++ snake_game.py
@@ -2,6 +2,7 @@
 import random 
 import curses 
 
+
 # setting the score out 
 score = 0
 
@@ -15,7 +16,9 @@ curses.curs_set(0)
 screen_height , screen_width = screen.getmaxyx() 
 
 # setting the height and width to can handle with .
-window = curses.getwin(screen_height , screen_width , 0 , 0)
+# window = curses.newwin(screen_height, screen_width , 0 , 0)   
+        # or 
+window = screen #  <= this 
 
 # allowing window to recive inputs and from keybourd by jey [ pad ( ) ]
 window.keypad(True)
@@ -44,6 +47,68 @@ window.addch(food[0] , food[1] , '')
 # set the iniotial movement .
 first_move = curses.KEY_RIGHT
 
+
+# for snake's color
+curses.start_color()
+curses.init_pair(1 , curses.COLOR_BLUE , curses.COLOR_BLACK)
+curses.init_pair(2 , curses.COLOR_RED , curses.COLOR_BLACK)
+
+
 # creating the loop that loops the game forever until the player quit or lose .
 while (True) :
+    # what is the next move after starting .
+    next_move = window.getch()
+
+    # if the user didn't pass any key the initial movement still exist .
+    first_move = next_move if next_move != -1 else first_move
+
+    # setting the next move by the input .
+    HEAD = [snake[0][0] , snake[0][1]] # inisialize the head if snake
+    if first_move == curses.KEY_RIGHT :
+        HEAD[1] += 1
+    elif first_move == curses.KEY_LEFT :
+        HEAD[1] -= 1
+    elif first_move == curses.KEY_UP :
+        HEAD[0] -= 1
+    elif first_move == curses.KEY_DOWN :
+        HEAD[0] += 1
+
+    # check if the Snake collided with wall or itself .
+    if HEAD in snake[ 1 : ] or HEAD[1] <= 0 or HEAD[1] >= screen_width or HEAD[0] <= 0 or HEAD[0] >= screen_height :
+        curses.endwin()
+        quit()
+
+# add a new head for the snake . 
+    snake.insert(0 , HEAD)
+
+    # check if the snake ate the food or not . 
+    if HEAD == food:
+        score += 10  
+
+        # creating food forever randomly .   
+        while True :
+            new_food = [
+                random.randint(2 , screen_height - 2), 
+                random.randint(2 , screen_width - 2)
+            ]
+
+            # if the food appares on snake's body remove it and generate a new one .
+            if new_food not in snake :  
+               food = new_food       
+               break 
+
+    # if the snake didn't eat the food remove the tail .
+    else : 
+        snake.pop()
+
+# refreshing content [ for drowing the new frame ]
+    window.clear()
+    window.addstr( 1 , 2 , f"score: {score}" )
+
+    window.addch(food[0] , food[1] , '' ,curses.color_pair(2))
     
+
+    for Y , X in snake :
+        window.addch( Y , X , '●' , curses.color_pair(1) )
+
+    window.refresh()
